10d37d09202cdc2b4ccc3a1c6ce2615a94e93483
Ultimate 365 <maidul365@gmail.com>
2024-12-19 20:46:06 +0530

    first commit

---
+++ src/modules/constants.js
@@ -0,0 +1,73 @@
+export const PREV6DA = 0.06;
+export const PREVDA = 0.1;
+export const DA = 0.14;
+export const NEXTDA = 0.14;
+export const INCREAMENT = 0.06;
+export const HRA = 0.12;
+export const BONUS = 5800;
+export const PREV_MDM_COST = 5.45;
+export const MDM_COST = 6.19;
+export const PP_STUDENTS = 6;
+export const PRIMARY_STUDENTS = 40;
+export const PRIMARY_BOYS = 22;
+export const PRIMARY_GIRLS = 18;
+export const TOTAL_STUDENTS = 46;
+export const SCHOOLNAME = 'UTTAR SEHAGORI PRIMARY SCHOOL';
+export const SCHOOLBENGALINAME = "উত্তর সেহাগড়ী প্রাথমিক বিদ্যালয়";
+export const SCHOOL_SPLASH_BENGALINAME = [
+  'উত্তর সেহাগড়ী',
+  'প্রাথমিক',
+  'বিদ্যালয়',
+];
+export const SCHOOLADDRESS =
+  'Vill.- Sehagori, P.O.- Khorop, P.S.- JoyPur, District- Howrah, PIN- 711401';
+export const SCHOOLBENGALIADDRESS =
+  'গ্রামঃ সেহাগড়ী, পোঃ- খড়িয়প, থানা- জয়পুর, জেলা- হাওড়া, পিন- ৭১১৪০১, আমতা পশ্চিমচক্র';
+export const WARD_NO = 'THALIA VEC-V';
+export const BLOCK = 'AMTA-II';
+export const SCHOOL_TYPE = 'GOVT. PRIMARY SCHOOL';
+export const CCH_NAME = [
+  {name: 'SHYAMALI MONDAL', gender: 'FEMALE', cast: 'GEN', payment: 'BANK'},
+  {name: 'LAKSHMI DAS', gender: 'FEMALE', cast: 'OBC', payment: 'BANK'},
+  ,
+];
+export const UDISE_CODE = 19160213201;
+export const HOI_MOBILE_NO = '9933684468';
+export const NGO_SHG = 'SUPRAVAT+DISHARI';
+const THIS_YEAR = new Date().getFullYear();
+export const CLASS_V_EXIST=false
+export const STUDENT_CLASSES = [
+  {name: 'PRE PRIMARY', value: 0, year: THIS_YEAR - 5},
+  {name: 'CLASS I', value: 1, year: THIS_YEAR - 6},
+  {name: 'CLASS II', value: 2, year: THIS_YEAR - 7},
+  {name: 'CLASS III', value: 3, year: THIS_YEAR - 8},
+  {name: 'CLASS IV', value: 4, year: THIS_YEAR - 9},
+  {name: 'CLASS V', value: 5, year: THIS_YEAR - 10},
+];
+export const VILL = 'SEHAGORI';
+export const PO = 'KHOROP';
+export const PS = 'JOYPUR';
+export const MOUZA = 'SEHAGORI';
+export const CIRCLE = 'AMTA WEST';
+export const MEDIUM = 'BENGALI';
+export const JLNO = '112';
+export const SCHNO = '72';
+export const LAST_DAY_OF_INSPECTION = '13-08-2024';
+export const SCHOOL_RECOGNITION_DATE = '23-04-1980';
+export const SCHOOL_AREA = '0.05 Acre';
+export const KHATIAN_NO = '21';
+export const PLOT_NO = '755';
+export const classWiseAge = [
+  {className: 'PRE PRIMARY', benClassName: 'প্রাক প্রাথমিক', age: 5},
+  {className: 'CLASS I', benClassName: 'প্রথম শ্রেনী', age: 6},
+  {className: 'CLASS II', benClassName: 'দ্বিতীয় শ্রেনী', age: 7},
+  {className: 'CLASS III', benClassName: 'তৃতীয় শ্রেনী', age: 8},
+  {className: 'CLASS IV', benClassName: 'চতুর্থ শ্রেনী', age: 9},
+  {className: 'CLASS V', benClassName: 'পঞ্চম শ্রেনী', age: 10},
+];
+export const TELEGRAM_TEACHER_GROUP = 'https://t.me/+LZsOjnE9mBAxZjI9';
+export const appVersion = 1.01;
+export const AppURL =
+  'https://drive.google.com/file/d/1FDcOHPETUb5iOA32SnOEUqPUiUkrljKE/view?usp=drive_link';
+export const WEBSITE = `https://uspry.vercel.app`;
+export const ADMISSION_STATUS="PY5G28aVQ3D3Q310GAKV"
